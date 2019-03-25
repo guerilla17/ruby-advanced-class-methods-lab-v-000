c9d827d22d986fb882e609b8861b368d8f3d3867
Gigi M <mmgamao1@gmail.com>
2019-03-25 17:07:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,18 +10,6 @@ class Song
     self.class.all << self
   end
 
-class Song
-  attr_accessor :name, :artist_name
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
-
   def self.create
     song = self.new
     #@@all << song
